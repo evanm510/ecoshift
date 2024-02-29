433090067b234ec45f43abff2f25d8871fa9be0a
evanm510 <evanm510@gmail.com>
2024-02-29 08:12:30 -0800

    updated to work with microservice

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,48 +1,14 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import "./../App.css";
+import React, { useState } from "react";
 
-const Results = ({ data }) => {
-  return (
-    <div>
-      {data.cost_per_mile && <div>Cost Per Mile: ${data.cost_per_mile}</div>}
-      {data.equivalent_mpg && <div>Equivalent MPG: {data.equivalent_mpg}</div>}
-      {data.annual_cost && <div>EV Annual Cost: ${data.annual_cost}</div>}
-      {data.gas_annual_cost && (
-        <div>Gas Annual Cost: ${data.gas_annual_cost}</div>
-      )}
-      {data.breakeven_time && (
-        <div>Break-even Time: {data.breakeven_time} years</div>
-      )}
-    </div>
-  );
-};
-
-export default function () {
+const InputForm = () => {
   const [calculationChoices, setCalculationChoices] = useState({
-    costPerMile: false,
+    evCostPerMile: false,
     equivalentMPG: false,
     annualCost: false,
     breakevenTime: false,
   });
 
-  useEffect(() => {
-    setFormValues((prevFormValues) => ({
-      ...prevFormValues,
-      calculationChoices: calculationChoices,
-    }));
-  }, [calculationChoices]);
-
-  const handleCheckboxChange = (event) => {
-    const { name, checked } = event.target;
-    setCalculationChoices((prevState) => ({
-      ...prevState,
-      [name]: checked,
-    }));
-  };
-
   const [formValues, setFormValues] = useState({
-    calculationChoices: calculationChoices,
     electricityPrice: "",
     evEfficiency: "",
     gasPrice: "",
@@ -50,51 +16,142 @@ export default function () {
     evVehiclePrice: "",
     taxCredit: false,
     gasVehiclePrice: "",
-    annualMileage: "",
+    annualMiles: "",
   });
 
   const [response, setResponse] = useState({});
 
   const handleChange = (event) => {
     const { name, value, type, checked } = event.target;
-    setFormValues({
-      ...formValues,
+    setFormValues((prev) => ({
+      ...prev,
       [name]: type === "checkbox" ? checked : value,
-    });
+    }));
   };
 
-  const handleSubmit = (event) => {
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setCalculationChoices((prev) => ({
+      ...prev,
+      [name]: checked,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    fetch("/calculate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formValues),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setResponse(data);
+    let responses = {};
+
+    if (calculationChoices.evCostPerMile) {
+      const endpoint = "http://localhost:3001/calcEvCostPerMile";
+      const data = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          electricityPrice: formValues.electricityPrice,
+          evEfficiency: formValues.evEfficiency,
+        }),
       })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+        .then((res) => res.json())
+        .then((data) => {
+          setResponse((prevResponses) => ({
+            ...prevResponses,
+            evCostPerMile: data,
+          }));
+        })
+        .catch((err) => console.error("Error:", err));
+    }
+
+    if (calculationChoices.equivalentMPG) {
+      const endpoint = "http://localhost:3001/calcEquivalentMpgCost";
+      const data = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          gasPrice: formValues.gasPrice,
+          electricityPrice: formValues.electricityPrice,
+          evEfficiency: formValues.evEfficiency,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setResponse((prevResponses) => ({
+            ...prevResponses,
+            equivalentMPG: data,
+          }));
+        })
+        .catch((err) => console.error("Error:", err));
+    }
+
+    if (calculationChoices.annualCost) {
+      const endpoint = "http://localhost:3001/calcEvAnnualCost";
+      const data = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          annualMiles: formValues.annualMiles,
+          electricityPrice: formValues.electricityPrice,
+          evEfficiency: formValues.evEfficiency,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setResponse((prevResponses) => ({
+            ...prevResponses,
+            annualCost: data,
+          }));
+        })
+        .catch((err) => console.error("Error:", err));
+    }
+
+    if (calculationChoices.breakevenTime) {
+      const endpoint = "http://localhost:3001/calcYearsUntilBreakeven";
+      const data = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          evPrice: formValues.evVehiclePrice,
+          gasVehiclePrice: formValues.gasVehiclePrice,
+          taxCredit: formValues.taxCredit,
+          gasPrice: formValues.gasPrice,
+          gasEfficiency: formValues.gasEfficiency,
+          annualMiles: formValues.annualMiles,
+          electricityPrice: formValues.electricityPrice,
+          evEfficiency: formValues.evEfficiency,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setResponse((prevResponses) => ({
+            ...prevResponses,
+            breakevenTime: data,
+          }));
+        })
+        .catch((err) => console.error("Error:", err));
+    }
   };
 
   return (
     <div>
-      <div>What would would like to calculate?</div>
+      <div>What would you like to calculate?</div>
       <div className="calculation-choice-container">
-        <div>
+        <label>
           Cost Per Mile
           <input
             type="checkbox"
-            name="costPerMile"
-            checked={calculationChoices.costPerMile}
+            name="evCostPerMile"
+            checked={calculationChoices.evCostPerMile}
             onChange={handleCheckboxChange}
           />
-        </div>
-        <div>
+        </label>
+        <label>
           Equivalent MPG
           <input
             type="checkbox"
@@ -102,8 +159,8 @@ export default function () {
             checked={calculationChoices.equivalentMPG}
             onChange={handleCheckboxChange}
           />
-        </div>
-        <div>
+        </label>
+        <label>
           Annual Cost
           <input
             type="checkbox"
@@ -111,8 +168,8 @@ export default function () {
             checked={calculationChoices.annualCost}
             onChange={handleCheckboxChange}
           />
-        </div>
-        <div>
+        </label>
+        <label>
           Breakeven Time
           <input
             type="checkbox"
@@ -120,125 +177,144 @@ export default function () {
             checked={calculationChoices.breakevenTime}
             onChange={handleCheckboxChange}
           />
-        </div>
+        </label>
       </div>
-      <div>
-        <form className="form-container">
-          {(calculationChoices.costPerMile ||
-            calculationChoices.equivalentMPG ||
-            calculationChoices.annualCost ||
-            calculationChoices.breakevenTime) && (
-            <>
-              <div>
-                <div className="user-input">
-                  Price of Electricity ($/kWh):
-                  <input
-                    type="text"
-                    name="electricityPrice"
-                    value={formValues.electricityPrice}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-              </div>
-              <div>
-                <div className="user-input">
-                  Efficiency of EV (miles/kWh):
-                  <input
-                    type="text"
-                    name="evEfficiency"
-                    value={formValues.evEfficiency}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-              </div>
-            </>
-          )}
-          {(calculationChoices.equivalentMPG ||
-            calculationChoices.breakevenTime) && (
-            <>
-              <div>
-                <div className="user-input">
-                  Price of Gas: $
-                  <input
-                    type="text"
-                    name="gasPrice"
-                    value={formValues.gasPrice}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-              </div>
-              <div>
-                <div className="user-input">
-                  Efficiency of Gas Vehicle (mpg):
-                  <input
-                    type="text"
-                    name="gasEfficiency"
-                    value={formValues.gasEfficiency}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-              </div>
-            </>
-          )}
-          {(calculationChoices.annualCost ||
-            calculationChoices.breakevenTime) && (
-            <div>
-              <div className="user-input">
-                Annual Mileage:
+      <form className="form-container" onSubmit={handleSubmit}>
+        {/* Conditionally render input fields based on selected calculation choices */}
+        {calculationChoices.evCostPerMile ||
+        calculationChoices.equivalentMPG ||
+        calculationChoices.annualCost ? (
+          <>
+            <div className="user-input">
+              <label>
+                Price of Electricity ($/kWh):
                 <input
                   type="text"
-                  name="annualMileage"
-                  value={formValues.annualMileage}
+                  name="electricityPrice"
+                  value={formValues.electricityPrice}
                   onChange={handleChange}
-                ></input>
-              </div>
+                />
+              </label>
             </div>
-          )}
-          {calculationChoices.breakevenTime && (
-            <>
-              <div>
-                <div className="user-input">
-                  Purchase Price of EV: $
-                  <input
-                    type="text"
-                    name="evVehiclePrice"
-                    value={formValues.evVehiclePrice}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-              </div>
-              <div>
-                <div className="user-input">
-                  Eligible for $7,500 tax credit?:
-                  <input
-                    type="checkbox"
-                    name="taxCredit"
-                    value={formValues.taxCredit}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-              </div>
-              <div>
-                <div className="user-input">
-                  Purchase Price of Gas Vehicle: $
-                  <input
-                    type="text"
-                    name="gasVehiclePrice"
-                    value={formValues.gasVehiclePrice}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-              </div>
-            </>
-          )}
-        </form>
+            <div className="user-input">
+              <label>
+                Efficiency of EV (miles/kWh):
+                <input
+                  type="text"
+                  name="evEfficiency"
+                  value={formValues.evEfficiency}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+          </>
+        ) : null}
+
+        {calculationChoices.equivalentMPG ||
+        calculationChoices.annualCost ||
+        calculationChoices.breakevenTime ? (
+          <>
+            <div className="user-input">
+              <label>
+                Price of Gas ($/gallon):
+                <input
+                  type="text"
+                  name="gasPrice"
+                  value={formValues.gasPrice}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="user-input">
+              <label>
+                Efficiency of Gas Vehicle (mpg):
+                <input
+                  type="text"
+                  name="gasEfficiency"
+                  value={formValues.gasEfficiency}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+          </>
+        ) : null}
+
+        {calculationChoices.breakevenTime ? (
+          <>
+            <div className="user-input">
+              <label>
+                Purchase Price of EV ($):
+                <input
+                  type="text"
+                  name="evVehiclePrice"
+                  value={formValues.evVehiclePrice}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="user-input">
+              <label>
+                Eligible for $7,500 tax credit?:
+                <input
+                  type="checkbox"
+                  name="taxCredit"
+                  checked={formValues.taxCredit}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="user-input">
+              <label>
+                Purchase Price of Gas Vehicle ($):
+                <input
+                  type="text"
+                  name="gasVehiclePrice"
+                  value={formValues.gasVehiclePrice}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+          </>
+        ) : null}
+
+        {calculationChoices.breakevenTime || calculationChoices.annualCost ? (
+          <div className="user-input">
+            <label>
+              Annual Mileage:
+              <input
+                type="text"
+                name="annualMiles"
+                value={formValues.annualMiles}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+        ) : null}
+
+        <button type="submit">Calculate</button>
+      </form>
+      {Object.keys(response).length > 0 && (
         <div>
-          <button onClick={handleSubmit}>
-            {Object.keys(response).length > 0 ? "ReCalculate" : "Calculate"}
-          </button>
+          <h2>Results</h2>
+          {response.evCostPerMile && (
+            <div>Cost Per Mile: ${response.evCostPerMile}</div>
+          )}
+          {response.equivalentMPG && (
+            <div>Equivalent MPG: {response.equivalentMPG}</div>
+          )}
+          {response.annualCost && (
+            <div>EV Annual Cost: ${response.annualCost}</div>
+          )}
+          {response.gasAnnualCost && (
+            <div>Gas Annual Cost: ${response.gasAnnualCost}</div>
+          )}
+          {response.breakevenTime && (
+            <div>Breakeven Time: {response.breakevenTime} years</div>
+          )}
         </div>
-      </div>
-      {Object.keys(response).length > 0 && <Results data={response} />}
+      )}
     </div>
   );
-}
+};
+
+export default InputForm;
---
+++ src/components/InputForm1.js
@@ -0,0 +1,244 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./../App.css";
+
+const Results = ({ data }) => {
+  return (
+    <div>
+      {data.cost_per_mile && <div>Cost Per Mile: ${data.cost_per_mile}</div>}
+      {data.equivalent_mpg && <div>Equivalent MPG: {data.equivalent_mpg}</div>}
+      {data.annual_cost && <div>EV Annual Cost: ${data.annual_cost}</div>}
+      {data.gas_annual_cost && (
+        <div>Gas Annual Cost: ${data.gas_annual_cost}</div>
+      )}
+      {data.breakeven_time && (
+        <div>Break-even Time: {data.breakeven_time} years</div>
+      )}
+    </div>
+  );
+};
+
+export default function () {
+  const [calculationChoices, setCalculationChoices] = useState({
+    costPerMile: false,
+    equivalentMPG: false,
+    annualCost: false,
+    breakevenTime: false,
+  });
+
+  useEffect(() => {
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      calculationChoices: calculationChoices,
+    }));
+  }, [calculationChoices]);
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setCalculationChoices((prevState) => ({
+      ...prevState,
+      [name]: checked,
+    }));
+  };
+
+  const [formValues, setFormValues] = useState({
+    calculationChoices: calculationChoices,
+    electricityPrice: "",
+    evEfficiency: "",
+    gasPrice: "",
+    gasEfficiency: "",
+    evVehiclePrice: "",
+    taxCredit: false,
+    gasVehiclePrice: "",
+    annualMileage: "",
+  });
+
+  const [response, setResponse] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    setFormValues({
+      ...formValues,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch("/calculate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formValues),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setResponse(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div>
+      <div>What would would like to calculate?</div>
+      <div className="calculation-choice-container">
+        <div>
+          Cost Per Mile
+          <input
+            type="checkbox"
+            name="costPerMile"
+            checked={calculationChoices.costPerMile}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+        <div>
+          Equivalent MPG
+          <input
+            type="checkbox"
+            name="equivalentMPG"
+            checked={calculationChoices.equivalentMPG}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+        <div>
+          Annual Cost
+          <input
+            type="checkbox"
+            name="annualCost"
+            checked={calculationChoices.annualCost}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+        <div>
+          Breakeven Time
+          <input
+            type="checkbox"
+            name="breakevenTime"
+            checked={calculationChoices.breakevenTime}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+      </div>
+      <div>
+        <form className="form-container">
+          {(calculationChoices.costPerMile ||
+            calculationChoices.equivalentMPG ||
+            calculationChoices.annualCost ||
+            calculationChoices.breakevenTime) && (
+            <>
+              <div>
+                <div className="user-input">
+                  Price of Electricity ($/kWh):
+                  <input
+                    type="text"
+                    name="electricityPrice"
+                    value={formValues.electricityPrice}
+                    onChange={handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <div className="user-input">
+                  Efficiency of EV (miles/kWh):
+                  <input
+                    type="text"
+                    name="evEfficiency"
+                    value={formValues.evEfficiency}
+                    onChange={handleChange}
+                  ></input>
+                </div>
+              </div>
+            </>
+          )}
+          {(calculationChoices.equivalentMPG ||
+            calculationChoices.breakevenTime) && (
+            <>
+              <div>
+                <div className="user-input">
+                  Price of Gas: $
+                  <input
+                    type="text"
+                    name="gasPrice"
+                    value={formValues.gasPrice}
+                    onChange={handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <div className="user-input">
+                  Efficiency of Gas Vehicle (mpg):
+                  <input
+                    type="text"
+                    name="gasEfficiency"
+                    value={formValues.gasEfficiency}
+                    onChange={handleChange}
+                  ></input>
+                </div>
+              </div>
+            </>
+          )}
+          {(calculationChoices.annualCost ||
+            calculationChoices.breakevenTime) && (
+            <div>
+              <div className="user-input">
+                Annual Mileage:
+                <input
+                  type="text"
+                  name="annualMileage"
+                  value={formValues.annualMileage}
+                  onChange={handleChange}
+                ></input>
+              </div>
+            </div>
+          )}
+          {calculationChoices.breakevenTime && (
+            <>
+              <div>
+                <div className="user-input">
+                  Purchase Price of EV: $
+                  <input
+                    type="text"
+                    name="evVehiclePrice"
+                    value={formValues.evVehiclePrice}
+                    onChange={handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <div className="user-input">
+                  Eligible for $7,500 tax credit?:
+                  <input
+                    type="checkbox"
+                    name="taxCredit"
+                    value={formValues.taxCredit}
+                    onChange={handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div>
+                <div className="user-input">
+                  Purchase Price of Gas Vehicle: $
+                  <input
+                    type="text"
+                    name="gasVehiclePrice"
+                    value={formValues.gasVehiclePrice}
+                    onChange={handleChange}
+                  ></input>
+                </div>
+              </div>
+            </>
+          )}
+        </form>
+        <div>
+          <button onClick={handleSubmit}>
+            {Object.keys(response).length > 0 ? "ReCalculate" : "Calculate"}
+          </button>
+        </div>
+      </div>
+      {Object.keys(response).length > 0 && <Results data={response} />}
+    </div>
+  );
+}
